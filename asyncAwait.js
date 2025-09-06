033ab26905877af93855245ef11ef6806d51de4e
PAKKURTHI NAGENDRA SAI <pakkurthinagendrasai@PAKKURTHIs-MacBook-Air.local>
2025-09-06 17:08:02 +0530

    k

---
+++ asyncAwait.js
@@ -0,0 +1,9 @@
+const sampleData=async()=>{
+    try{
+    let response=await fetch("https://www.google.com")
+    console.log(response)
+}catch (error){
+    console.log("error" + error)
+}
+}
+sampleData()

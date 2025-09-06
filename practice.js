066a81ceae152c8a21f30b79502c1f8f4f827e84
PAKKURTHI NAGENDRA SAI <pakkurthinagendrasai@MacBookAir.mshome.net>
2025-09-06 12:35:30 +0530

    s

--- practice.js
+++ practice.js
@@ -1,1 +1,17 @@
 console.log("Hello, World!");
+let name = "de";
+let x;
+console.log(x)
+console.log(name);
+console.log(typeof name)
+let isLogged=false;
+console.log(typeof isLogged);
+console.log(typeof x);
+let arr = [1,2,3,4,5]
+console.log(arr);
+console.log(typeof arr);
+let obj = {
+    name:"de",
+}
+console.log(arr.name)
+console.log(typeof arr);
